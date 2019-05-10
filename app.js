812dfd719cbbdc4fe606d82e1a0e718512374dc4
Katharina Nahm <katja_nahm@web.de>
2019-05-10 15:34:13 +0000

    update dbUrl

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var reviewRoutes     = require("./routes/reviews"),
     playgroundRoutes = require("./routes/playgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect('mongodb://localhost:27017/playground_app', { useNewUrlParser: true, useCreateIndex: true });
+// mongoose.connect('mongodb://localhost:27017/playground_app', { useNewUrlParser: true, useCreateIndex: true });
+// mongoose.connect('mongodb+srv://dbUser:slr94AK@cluster0-bfcz2.mongodb.net/test?retryWrites=true', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useCreateIndex: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
